6c9f8ac355ff1ed3ee1c0ebe697aad2d9329f0b8
Oliinka <olga.titzenthaler@gmail.com>
2023-10-14 20:51:56 +0200

    du

---
+++ kodim_02_slicing/02_midle_lnumber_on_list.py
@@ -0,0 +1,10 @@
+list = [1, 2, 4, 7, 6, 0, 1, 2]
+
+print(list[:3])
+mid_number=int(len(list)/2)
+print(mid_number)
+
+if mid_number%2==0:
+    print(list[mid_number-1]) #sudy pocet cisel, rvni z dvojice "print(list[mid_number])" druhe z dvojice
+else:
+    print(list[mid_number]) #lichy pocet cisel
